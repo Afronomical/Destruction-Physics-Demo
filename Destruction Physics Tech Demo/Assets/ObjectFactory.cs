5609cc24d9ccea5078d6d7d30b7d103cbbe0bce3
Somi Euler-Ajayi <66644410+Afronomical@users.noreply.github.com>
2025-10-05 00:48:45 +0100

    Object Factory created
    Created an asset to spew out objects on an interval.
    Parameters include:
    -Max number
    -Creation interval
    -Spawn Offsets

--- Destruction Physics Tech Demo/Assets/ObjectFactory.cs
+++ Destruction Physics Tech Demo/Assets/ObjectFactory.cs
@@ -1,6 +1,61 @@
+using System.Collections;
 using UnityEngine;
 
 public class ObjectFactory : MonoBehaviour
 {
-    [SerializeField] private float spawnInterval = 1.0f;
+
+    [Tooltip("Put the object you would like to spawn in here")]
+    [SerializeField] public GameObject spawnObject;
+
+    [Space]
+
+    [Tooltip("Time between each new object's creation (in seconds)")]
+    [Min(0)][SerializeField] private float spawnInterval = 1.0f;
+
+    [Tooltip("The limit for spawned objects this factory can make")]
+    [Min(-1)][SerializeField] private int maxSpawned = 100;
+    private int numberSpawned = 0;
+
+    [Space]
+
+    [Tooltip("Spawn the object at a slight offset to this objects position")]
+    [SerializeField] public Vector3 spawnOffset = Vector3.zero;
+
+    bool spawning = false;
+
+    IEnumerator SpawnObject()
+    {
+    
+    //Checks if an object is being spawned already (Cooldown is active)sa
+        if (spawning) { yield break ; };
+
+        
+        //Ensures object count is kept below maximum.
+        //      If maxSpawned is -1 will spawn continuosly
+        if (maxSpawned < 0 || numberSpawned < maxSpawned)
+        {
+            spawning = true;
+
+            Instantiate(spawnObject, transform.position + spawnOffset, spawnObject.transform.rotation);
+            yield return new WaitForSeconds(spawnInterval);
+            ++numberSpawned;
+
+            spawning = false;
+        }
+        
+
+        
+        
+
+    }
+
+    private void Update()
+    {
+        Spawn();
+    }
+
+    void Spawn()
+    { 
+            StartCoroutine(SpawnObject());
+    }
 }
